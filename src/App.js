b7b3c0daa1530f44047fd1d7ad75b209067bf9ad
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2021-10-21 12:21:02 +0300

    lesson-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Message } from "./components/Message";
+
+const text = "этот текст передали пропсом и стилизовали через css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message text={text} />
     </div>
   );
 }
---
+++ src/components/Message.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Message = (props) => {
+  return (
+    <div>
+      <p className="main__text">{props.text}</p>
+    </div>
+  );
+};
